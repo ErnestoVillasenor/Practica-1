2c0e4e628f63ad4f9367daff140d66281da72d43
ErnestoVillasenor <jose.villasenor0349@alumnos.udg.mx>
2025-08-16 20:30:22 -0600

    Add files via upload

---
+++ P1.m
@@ -0,0 +1,10 @@
+% Parámetros del sistema
+
+Ip   = 0.0079;      % kg*m^2
+Mc   = 0.7031;      % kg
+lp   = 0.3302;      % m
+Mp   = 0.23;        % kg
+Fc   = 0;           % N
+Beq  = 4.3;         % Ns/m
+g    = 9.81;        % m/s^2
+Bp   = 0.0024;      % Nms/rad
